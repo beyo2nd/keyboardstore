e049c9f81db425d0994edd80e8dee6c90996702e
Max <max_antimonov@mail.ru>
2025-02-18 03:53:50 +0300

    new file:   .gitignore 	modified:   db.sqlite3 	modified:   keyboardstore/urls.py 	modified:   main/__pycache__/urls.cpython-313.pyc 	modified:   main/__pycache__/views.cpython-313.pyc 	modified:   main/static/css/base.css 	modified:   main/templates/main/base.html 	modified:   main/templates/main/index/index.html 	modified:   main/templates/main/product/detail.html 	new file:   main/templates/main/product/list.html 	deleted:    main/templates/main/product/login.html 	deleted:    main/templates/main/product/registration.html 	modified:   main/urls.py 	modified:   main/views.py

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,9 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.popular_list, name='popular'),
+    path('shop/', views.product_list, name='product_list'),
+    path('shop/category/<slug:category_slug>/', views.product_list,
+         name='product_list_by_category'),
     path('<slug:slug>/', views.product_detail,
          name='product_detail'),
 ]
